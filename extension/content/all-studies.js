b83cec4ccf00e62b1a4668c2966dbe223399d7e6
Jono X <jono@mozilla.com>
2011-08-23 22:06:06 -0700

    Mobile Test Pilot - Redid the all-your-studies page to be more attractive and informative and to look sort of like the XUL window version on desktop.

--- extension/content/all-studies.js
+++ extension/content/all-studies.js
@@ -36,11 +36,20 @@
 
 // for the HTML version
 
+const NO_STUDIES_IMG = "chrome://testpilot/skin/testPilot_200x200.png";
+const PROPOSE_STUDY_URL =
+  "https://wiki.mozilla.org/Labs/Test_Pilot#For_researchers";
+
 function _sortNewestFirst(experiments) {
     experiments.sort(
       function sortFunc(a, b) {
+        if (a.status != b.status) {
+          return a.status - b.status;
+          // Ascending order of status - will put finished below in-progress
+          // and put canceled studies at the bottom.
+        }
         if (a.endDate && b.endDate) {
-          return b.endDate - a.endDate;
+          return b.endDate - a.endDate; // Descending order of date
         }
         if (a.publishDate && b.publishDate) {
           if (isNaN(a.publishDate) || isNaN(b.publishDate)) {
@@ -58,8 +67,14 @@ function fillAllStudiesPage() {
   Components.utils.import("resource://testpilot/modules/Observers.js");
   Components.utils.import("resource://testpilot/modules/setup.js");
   Components.utils.import("resource://testpilot/modules/tasks.js");
-  //this._stringBundle = document.getElementById("testpilot-stringbundle");
 
+  let stringBundle =
+      Components.classes["@mozilla.org/intl/stringbundle;1"].
+        getService(Components.interfaces.nsIStringBundleService).
+          createBundle("chrome://testpilot/locale/main.properties");
+
+  let loadingMsg =  document.getElementById("still-loading-msg");
+  loadingMsg.innerHTML = stringBundle.GetStringFromName("testpilot.statusPage.loading");
 
   // Are we done loading tasks?
   if (!TestPilotSetup.startupComplete || TestPilotSetup.getAllTasks().length == 0) {
@@ -70,33 +85,104 @@ function fillAllStudiesPage() {
   }
 
   // hide the 'loading' msg
-  window.document.getElementById("still-loading-msg").innerHTML = "";
+  loadingMsg.innerHTML = "";
 
   // clear the table
   let table = window.document.getElementById("studies-list");
   table.innerHTML = "";
 
-  let experiments = _sortNewestFirst(TestPilotSetup.getAllTasks());
+  let experiments = TestPilotSetup.getAllTasks();
+  if (experiments.length == 0) {
+    // Show the message about no studies existing yet:
+    // TODO test this part
+    let span = document.createElement("span");
+    span.innerHTML = stringBundle.GetStringFromName("testpilot.studiesWindow.noStudies");
+
+    let link = document.createElement("a");
+    link.setAttribute("href", PROPOSE_STUDY_URL);
+    link.innerHTML = stringBundle.GetStringFromName("testpilot.studiesWindow.proposeStudy");
+
+    let img = document.createElement("img");
+    img.setAttribute("src", NO_STUDIES_IMG);
+    loadingMsg.appendChild(span);
+    loadingMsg.appendChild(link);
+    loadingMsg.appendChild(img);
+    return;
+  }
+  experiments = _sortNewestFirst(experiments);
 
   for (let i = 0; i < experiments.length; i++) {
     let task = experiments[i];
     let newRow = document.createElement("tr");
 
+    let img = document.createElement("img");
+    img.setAttribute("class", "results-thumbnail");
+    img.setAttribute("src", task.thumbnail);
     let newCell = document.createElement("td");
-    newCell.innerHTML = task.title;
-    newRow.appendChild(newCell);
-    newCell = document.createElement("td");
-    newCell.innerHTML = task.summary;
+    newCell.appendChild(img);
     newRow.appendChild(newCell);
 
     let link = document.createElement("a");
     link.setAttribute("href", task.defaultUrl);
-    link.innerHTML = "More Info";
-
+    link.setAttribute("class", "study-title");
+    link.innerHTML = task.title;
     newCell = document.createElement("td");
     newCell.appendChild(link);
     newRow.appendChild(newCell);
 
+    let p = document.createElement("p");
+    p.setAttribute("class", "ensmallened");
+    img = null;
+    switch (task.status) {
+    case TaskConstants.STATUS_CANCELLED:
+      img = document.createElement("img");
+      img.setAttribute("src", "chrome://testpilot/skin/status-ejected.png");
+      p.innerHTML = stringBundle.GetStringFromName("testpilot.studiesWindow.canceledStudy");
+      break;
+    case TaskConstants.STATUS_SUBMITTED:
+      img = document.createElement("img");
+      img.setAttribute("src", "chrome://testpilot/skin/status-completed.png");
+      p.innerHTML = stringBundle.GetStringFromName("testpilot.studiesWindow.thanksForContributing");
+      break;
+    case TaskConstants.STATUS_MISSED:
+      img = document.createElement("img");
+      img.setAttribute("src", "chrome://testpilot/skin/status-missed.png");
+      p.innerHTML = stringBundle.GetStringFromName("testpilot.studiesWindow.missedStudy");
+      break;
+    case TaskConstants.STATUS_FINISHED: // TODO submit button here?
+      p.innerHTML = stringBundle.formatStringFromName(
+        "testpilot.studiesWindow.finishedOn",
+        [(new Date(task.endDate)).toLocaleDateString()], 1);
+      break;
+    case TaskConstants.STATUS_IN_PROGRESS:
+    case TaskConstants.STATUS_STARTING:
+      p.innerHTML = stringBundle.formatStringFromName(
+        "testpilot.studiesWindow.willFinish",
+        [(new Date(task.endDate)).toLocaleDateString()], 1);
+      break;
+    case TaskConstants.STATUS_PENDING:
+    case TaskConstants.STATUS_NEW:
+      p.innerHTML = stringBundle.formatStringFromName(
+        "testpilot.studiesWindow.willStart",
+        [(new Date(task.startDate)).toLocaleDateString()], 1);
+      break;
+    }
+    newCell = document.createElement("td");
+    if (img) {
+      newCell.appendChild(img);
+      newCell.appendChild(document.createElement("br"));
+    }
+    newCell.appendChild(p);
+    newRow.appendChild(newCell);
+
+    table.appendChild(newRow);
+
+    newRow = document.createElement("tr");
+    newCell = document.createElement("td");
+    newCell.setAttribute("colspan", 3);
+    newCell.setAttribute("class", "underlined-cell");
+    newCell.innerHTML = task.summary;
+    newRow.appendChild(newCell);
     table.appendChild(newRow);
   }
 }
